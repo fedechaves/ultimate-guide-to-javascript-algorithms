52809a6b9e680d39b964c330e089f21f6b6dfca3
fedechaves <federicochavesf@gmail.com>
2023-01-30 19:27:32 -0300

    merge Arrays

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,18 +4,46 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function mergeArrays(...arrays) {
+//combining arrays
 
+function mergeArrays(...arrays) {
     let jointArray = []
-    
     arrays.forEach(array => {
         jointArray = [...jointArray, ...array]
     });
-
+    
     return [...new Set([...jointArray])]
 
-    
+    //another way to return array w/o duplicates
+    //Array.from(new Set([...jointArray]))
+
+    //with fileter method
+    // const uniqueArray = jointArray.filter((item,index) => jointArray.indexOf(item) === index)
+    // return uniqueArray
+
+    //reduce methods
+    // const uniqueArray = jointArray.reduce((newArray, item) =>{
+    //     if (newArray.includes(item)){
+    //         return newArray
+    //     } else {
+    //         return [...newArray, item]
+    //     }
+    // }, [])
+    // return uniqueArray
 }
+
+// function mergeArrays(...arrays) {
+
+//     let jointArray = []
+    
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+
+//     return [...new Set([...jointArray])]
+
+    
+// }
 
 
 module.exports = mergeArrays
